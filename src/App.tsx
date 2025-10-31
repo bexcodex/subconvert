df10fee5bdcf2708927312a52b997b0605e03f1c
bexcodex <bexcode0@gmail.com>
2025-10-31 18:07:17 +0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center space-x-4">
-              <img src="/icon.jpg" className="h-8 w-8" alt="icon" />
+              <img src="/icon.png" className="h-8 w-8" alt="icon" />
               <h1 className="text-xl font-bold text-text-heading">
                 Clash Converter
               </h1>
